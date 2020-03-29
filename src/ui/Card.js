10b7de5b160a7c2be4e405cec4f3fa5f4d6210fd
marco_dca <marcodecara@gmail.com>
2020-03-29 16:28:43 +0200

    mocking gestures

---
+++ src/ui/Card.js
@@ -0,0 +1,83 @@
+import React, { useState, useRef, useEffect } from "react"
+import { motion, useMotionValue } from "framer-motion"
+import chroma from "chroma-js"
+import styled from "styled-components"
+
+const Card = ({ color, setPosition, moveItem, i, children }) => {
+  const [isDragging, setDragging] = useState(false)
+
+  // We'll use a `ref` to access the DOM element that the `motion.li` produces.
+  // This will allow us to measure its height and position, which will be useful to
+  // decide when a dragging element should switch places with its siblings.
+  const ref = useRef(null)
+
+  // By manually creating a reference to `dragOriginY` we can manipulate this value
+  // if the user is dragging this DOM element while the drag gesture is active to
+  // compensate for any movement as the items are re-positioned.
+  const dragOriginY = useMotionValue(0)
+
+  // Update the measured position of the item so we can calculate when we should rearrange.
+  useEffect(() => {
+    setPosition(i, {
+      height: ref.current.offsetHeight,
+      top: ref.current.offsetTop,
+    })
+  })
+
+  console.log(isDragging)
+
+  return (
+    <>
+      <StyledCard
+        ref={ref}
+        initial={false}
+        // If we're dragging, we want to set the zIndex of that item to be on top of the other items.
+        animate={isDragging ? onTop : flat}
+        style={{ background: "pink", height: "60px" }}
+        whileHover={{ scale: 1.03 }}
+        whileTap={{ scale: 1.12 }}
+        drag="y"
+        dragOriginY={dragOriginY}
+        dragConstraints={{ top: 0, bottom: 0 }}
+        dragElastic={1}
+        onDragStart={() => setDragging(true)}
+        onDragEnd={() => setDragging(false)}
+        onDrag={(e, { point }) => moveItem(i, point.y)}
+        positionTransition={({ delta }) => {
+          if (isDragging) {
+            // If we're dragging, we want to "undo" the items movement within the list
+            // by manipulating its dragOriginY. This will keep the item under the cursor,
+            // even though it's jumping around the DOM.
+            dragOriginY.set(dragOriginY.get() + delta.y)
+          }
+
+          // If `positionTransition` is a function and returns `false`, it's telling
+          // Motion not to animate from its old position into its new one. If we're
+          // dragging, we don't want any animation to occur.
+          return !isDragging
+        }}
+      >
+        {children}
+      </StyledCard>
+    </>
+  )
+}
+
+const StyledCard = styled(motion.li)`
+  padding: 0;
+  margin: 0;
+  border-radius: 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+  width: 100%;
+  position: relative;
+`
+
+// Spring configs
+const onTop = { zIndex: 1 }
+const flat = {
+  zIndex: 0,
+  transition: { delay: 0.3 },
+}
+
+export default Card
--- src/ui/CardsDisplay.js
+++ src/ui/CardsDisplay.js
@@ -1,9 +1,47 @@
-import React, { useState } from "react"
+import React, { useState, useReducer, useEffect, useRef } from "react"
 import useLocalStorage from "./hooks/useLocalStorage"
+import move from "array-move"
+import findIndex from "./utils/findIndex"
+import Card from "./Card"
+import styled from "styled-components"
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-card":
+      return [...state, action.payload]
+    case "delete-card":
+      return state.filter(card => card.id !== action.payload)
+    case "rearrange-cards":
+      return action.payload
+    default:
+      return state
+  }
+}
 
 const CardsDisplay = () => {
-  const [cardsData, setCardsData] = useLocalStorage("cardsData", "[]")
-  const [newCard, setNewCard] = useState({ title: "" })
+  const [cardsData, setCardsData] = useLocalStorage("cardsData", [])
+  const [newCard, setNewCard] = useState({ title: "", id: null })
+  const [state, dispatch] = useReducer(reducer, cardsData)
+
+  useEffect(() => {
+    setCardsData(state)
+  }, [state])
+
+  //   console.log(state)
+
+  //All mock up
+
+  const positions = useRef([]).current
+  console.log(positions)
+  const setPosition = (i, offset) => (positions[i] = offset)
+  const moveItem = (i, dragOffset) => {
+    const targetIndex = findIndex(i, dragOffset, positions)
+    if (targetIndex !== i)
+      dispatch({
+        type: "rearrange-cards",
+        payload: move(state, i, targetIndex),
+      })
+  }
 
   return (
     <div>
@@ -20,18 +58,34 @@ const CardsDisplay = () => {
           type="submit"
           onClick={e => {
             e.preventDefault()
-            setCardsData(prev => JSON.stringify([...JSON.parse(prev), newCard]))
-            setNewCard({ title: "" })
+
+            dispatch({
+              type: "add-card",
+              payload: { ...newCard, id: Math.floor(Math.random() * 100) },
+            })
+            setNewCard({ title: "", id: null })
           }}
         >
           Create Card
         </button>
       </form>
-      {JSON.parse(cardsData).map((el, i) => (
-        <p key={i}>{el.title}</p>
-      ))}
+      <Ul>
+        {state.map((el, i) => (
+          <Card key={el.id} i={i} setPosition={setPosition} moveItem={moveItem}>
+            {el.title}
+          </Card>
+        ))}
+      </Ul>
     </div>
   )
 }
+
+const Ul = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  position: relative;
+  width: 300px;
+`
 
 export default CardsDisplay
